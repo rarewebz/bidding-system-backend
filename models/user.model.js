a3ec58c0545761970ce78b4ff3ff2d2ad861b44f
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-01-17 20:34:15 +0530

    develop user model schema

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose =  require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['ACTIVE', 'INACTIVE', 'DELETED'],
+        required: true
+    }
+})
+
+module.exports = mongoose.model('user', userSchema)
