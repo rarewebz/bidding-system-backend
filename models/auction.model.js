e053e63ffa28d7d7a07638728d53f90369ee3242
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-01-19 16:41:49 +0530

    develop auction schema

---
+++ models/auction.model.js
@@ -0,0 +1,50 @@
+const mongoose =  require('mongoose')
+const { Decimal128 } = require('bson')
+
+const auctionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    initialprice: {
+        type: Decimal128,
+        required: true
+    },
+    startdate: {
+        type: Date,
+        required: true
+    },
+    enddate: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: [String],
+        required: true
+    },
+    ownerId: {
+        type: String,
+        required: true
+    },
+    winnerId: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['ACTIVE', 'INACTIVE', 'DELETED', 'ENDED', 'CLOSED'],
+        required: true,
+        default: 'ACTIVE'
+    }
+})
+
+module.exports = mongoose.model('auction', auctionSchema)
