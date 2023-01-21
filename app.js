fd06916267e7e36c4b36aa881ccd4ee4317f2b94
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-01-21 18:31:04 +0530

    set origin headers

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.json())
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
 app.use(express.static('public'))
+app.use(cors({
+    origin: '*'
+}))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    next()
+})
 
 // ---- import mongooes ----
 const mongoose = require('mongoose')
@@ -37,9 +46,6 @@ const myRouter = require('./routes/my.route')
 app.use('/my', myRouter)
 
 // ---- start the server ----
-app.use(cors({
-    origin: '*'
-}))
 app.listen(3000, () => {
     console.log("Backend server started...")
 })
