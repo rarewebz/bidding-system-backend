7ff06839dd3cd4475c5c8b7c0862de44a05732d1
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-01-21 17:14:55 +0530

    fix bug at bid save

--- routes/bid.route.js
+++ routes/bid.route.js
@@ -5,9 +5,11 @@ const config = require('../config/config')
 const express = require('express')
 const UserModel = require('../models/user.model')
 const BidModel = require('../models/bid.model')
+const AuctionModel = require('../models/auction.model')
 const router = express.Router()
 
 const getUser = async (req, res, next) => {
+    console.log('Get User Middleware')
     let user
     try {
         user = await UserModel.findById(req.body.userId)
@@ -21,12 +23,36 @@ const getUser = async (req, res, next) => {
     next()
 }
 
-router.get('/auction/:id', getUser, async (req, res) => {
+const getAuction = async (req, res, next) => {
+    console.log('Get Auction Middleware')
+    let auction
     try {
+        auction = await AuctionModel.findById(req.params.id)
+        if(auction == null) {
+            return res.status(404).json({message: "Auction not found"})
+        }
+    } catch(e) {
+        return res.status(500).json({message: e.message})
+    }
+    res.auction = auction
+    next()
+}
+
+router.post('/auction/:id', getUser, getAuction, async (req, res) => {
+
+    console.log('res: ', res)
+
+    try {
+        if(res.auction.ownerId === res.user._id)
+            res.json({
+                success: false,
+                body: null,
+                message: "Auction owner can't bid"
+            })
         const bid = new BidModel({
-            auctionId: req.params.status,
+            auctionId: req.params.id,
             userId: req.body.userId,
-            username: res.user.username,
+            username: res.user.firstname,
             bid: req.body.bid
         })
         const result = await bid.save()
@@ -36,6 +62,7 @@ router.get('/auction/:id', getUser, async (req, res) => {
             message: 'Bid saved successfully!'
         })
     } catch (e) {
+        console.log(e)
         res.status(400).json({
             success: false,
             message: 'Invalid inputs.'
