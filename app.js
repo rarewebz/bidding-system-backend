b7a7e696ae007941baa3ffe72225b535c6eedba3
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-01-23 03:38:12 +0530

    develop socket for biding

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.use(express.json())
 
 // ---- import socket.io ----
 let server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+    allowEIO3: true,
+    cors: {
+        origin: true,
+        credentials: true
+    },
+});
 
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
@@ -50,9 +56,167 @@ app.use('/bid', bidRouter)
 const myRouter = require('./routes/my.route')
 app.use('/my', myRouter)
 
-require('./routes/io-handler')(io);
+// require('./routes/io-handler')(io);
+
+var STATIC_CHANNELS = [{
+    name: 'Global chat',
+    participants: 0,
+    id: 1,
+    sockets: []
+}, {
+    name: 'Funny',
+    participants: 0,
+    id: 2,
+    sockets: []
+}];
+
+let CURRENT_CHANNELS = [
+    {
+        name: 'Funny',
+        participants: 0,
+        id: '10000',
+        sockets: []
+    }
+]
+
+// io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
+//     console.log('new client connected');
+//     socket.emit('connection', null);
+//     socket.on('channel-join', id => {
+//         console.log('channel join', id);
+//         STATIC_CHANNELS.forEach(c => {
+//             if (c.id === id) {
+//                 if (c.sockets.indexOf(socket.id) == (-1)) {
+//                     c.sockets.push(socket.id);
+//                     c.participants++;
+//                     io.emit('channel', c);
+//                 }
+//             } else {
+//                 let index = c.sockets.indexOf(socket.id);
+//                 if (index != (-1)) {
+//                     c.sockets.splice(index, 1);
+//                     c.participants--;
+//                     io.emit('channel', c);
+//                 }
+//             }
+//         });
+//
+//         return id;
+//     });
+//     socket.on('send-message', message => {
+//         io.emit('message', message);
+//     });
+//
+//     socket.on('disconnect', () => {
+//         STATIC_CHANNELS.forEach(c => {
+//             let index = c.sockets.indexOf(socket.id);
+//             if (index != (-1)) {
+//                 c.sockets.splice(index, 1);
+//                 c.participants--;
+//                 io.emit('channel', c);
+//             }
+//         });
+//     });
+//
+// });
+
+
+const AuctionModal = require('./models/auction.model')
+
+io.on('connection', (socket) => { // socket object may be used to send specific messages to the new connected client
+    console.log('new client connected');
+    socket.emit('connection', null);
+
+    socket.on('channel-join', async data => {
+        let id = data.auctionId
+        let userId = data.userId
+        console.log('channel join', id);
+        let result = await CURRENT_CHANNELS.findIndex(e => e.id == id)
+        if(result < 0) {
+            console.log('channel join - 1: ', socket.id);
+            const result = await AuctionModal.findById(id)
+            if(result) {
+                let sockets = []
+                sockets.push(socket.id)
+                let newChannel = {
+                    name: result.name,
+                    participants: 1,
+                    id: result._id,
+                    sockets: sockets
+                }
+                CURRENT_CHANNELS.push(newChannel)
+                io.emit('channel', newChannel)
+            }
+        } else {
+            console.log('channel join - 2: ', socket.id);
+                CURRENT_CHANNELS.forEach(c => {
+                                if (c.id == id) {
+                                    console.log("socket: ", socket.id)
+                                    console.log("socket idx: ", c.sockets.indexOf(socket.id))
+                                    if (c.sockets.indexOf(socket.id) == (-1)) {
+                                        c.sockets.push(socket.id);
+                                        c.participants++;
+                                        io.emit('channel', c);
+                                    }
+                                }
+                })
+        }
+
+
+        // CURRENT_CHANNELS.forEach(async c => {
+        //     if (c.id === id) {
+        //         console.log("socket: ", socket.id)
+        //         if (c.sockets.indexOf(socket.id) == (-1)) {
+        //             c.sockets.push(socket.id);
+        //             c.participants++;
+        //             io.emit('channel', c);
+        //         }
+        //     } else {
+        //         const result = await AuctionModal.findById(id)
+        //         if(result) {
+        //             let sockets = []
+        //             sockets.push(socket.id)
+        //             let newChannel = {
+        //                 name: result.name,
+        //                 participants: 1,
+        //                 id: result._id,
+        //                 sockets: sockets
+        //             }
+        //             CURRENT_CHANNELS.push(newChannel)
+        //             io.emit('channel', newChannel)
+        //         }
+        //         let index = c.sockets.indexOf(socket.id);
+        //         if (index != (-1)) {
+        //             c.sockets.splice(index, 1);
+        //             c.participants--;
+        //             io.emit('channel', c);
+        //         }
+        //     }
+        // });
+
+        return id;
+    });
+
+    socket.on('bid', async message => {
+        let channel = await CURRENT_CHANNELS.findIndex(e => e.id == message.channel_id)
+        console.log("CURRENT_CHANNELS[channel]?.sockets: ", CURRENT_CHANNELS[channel]?.sockets)
+        io.sockets.in(CURRENT_CHANNELS[channel]?.sockets).emit('bid-listener', message);
+    });
+
+    socket.on('disconnect', () => {
+        STATIC_CHANNELS.forEach(c => {
+            let index = c.sockets.indexOf(socket.id);
+            if (index != (-1)) {
+                c.sockets.splice(index, 1);
+                c.participants--;
+                io.emit('channel', c);
+            }
+        });
+    });
+
+});
 
 // ---- start the server ----
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Backend server started...")
 })
--- routes/io-handler.js
+++ routes/io-handler.js
@@ -12,48 +12,51 @@ const ee = new EventEmitter();
 const namespacesCreated = {}; // will store the existing namespaces
 
 module.exports = (io) => {
-    // io.sockets.on('connection', (socket) => {
+    io.sockets.on('connection', (socket) => {
+
+        console.log('test')
+
+        const { ns } = url.parse(socket.handshake.url, true).query;
+        let matched = false;
+
+        if (!ns) { // if there is not a ns in query disconnect the socket
+            socket.disconnect();
+            return { err: 'ns not provided' };
+        }
+
+
+        Object.keys(routes).forEach((name) => {
+            const matches = ns.match(routes[name]);
+
+            if (matches) {
+                matched = true;
+                if (!namespacesCreated[ns]) { // check if the namespace was already created
+                    namespacesCreated[ns] = true;
+                    io.of(ns).on('connection', (nsp) => {
+                        const evt = `dynamic.group.${name}`; // emit an event four our group of namespaces
+                        ee.emit(evt, nsp, ...matches.slice(1, matches.length));
+                    });
+                }
+            }
+        });
+
+        if (!matched) { // if there was no match disconnect the socket
+            socket.disconnect();
+        }
+    });
+
+
+    // io.on("connection", (socket) => {
+    //     console.log(`User Connected: ${socket.id}`);
     //
-    //     const { ns } = url.parse(socket.handshake.url, true).query;
-    //     let matched = false;
-    //
-    //     if (!ns) { // if there is not a ns in query disconnect the socket
-    //         socket.disconnect();
-    //         return { err: 'ns not provided' };
-    //     }
-    //
-    //     Object.keys(routes).forEach((name) => {
-    //         const matches = ns.match(routes[name]);
-    //
-    //         if (matches) {
-    //             matched = true;
-    //             if (!namespacesCreated[ns]) { // check if the namespace was already created
-    //                 namespacesCreated[ns] = true;
-    //                 io.of(ns).on('connection', (nsp) => {
-    //                     const evt = `dynamic.group.${name}`; // emit an event four our group of namespaces
-    //                     ee.emit(evt, nsp, ...matches.slice(1, matches.length));
-    //                 });
-    //             }
-    //         }
+    //     socket.on("join_room", (data) => {
+    //         socket.join(data);
     //     });
     //
-    //     if (!matched) { // if there was no match disconnect the socket
-    //         socket.disconnect();
-    //     }
+    //     socket.on("send_message", (data) => {
+    //         socket.to(data.room).emit("receive_message", data);
+    //     });
     // });
-
-
-    io.on("connection", (socket) => {
-        console.log(`User Connected: ${socket.id}`);
-
-        socket.on("join_room", (data) => {
-            socket.join(data);
-        });
-
-        socket.on("send_message", (data) => {
-            socket.to(data.room).emit("receive_message", data);
-        });
-    });
 
 
     return ee; // we can return the EventEmitter to be used in our server.js file
